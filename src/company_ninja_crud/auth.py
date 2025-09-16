c78445213dc74b1b910b596580c556daf5de1093
Chris Muthig <camuthig@gmail.com>
2025-09-16 17:50:51 -0600

    Add request.user support to the auth implementation

--- src/company_ninja_crud/auth.py
+++ src/company_ninja_crud/auth.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AnonymousUser
 from ninja.security import HttpBearer
 
 
@@ -18,5 +19,9 @@ class DummyAuthBearer(HttpBearer):
         # On success, return the user
         # This must be "true" like, and Django Ninja will add this to the `request.auth` field.
         user = get_user_model().objects.filter(id=user_id).first()
+
+        # This forces the user object onto the `request.user` property so that it is available for things like the
+        # `permission_required` decorator.
+        request.user = user or AnonymousUser()
 
         return user
